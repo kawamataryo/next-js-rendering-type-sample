8d2e3f8552f9f595f1dc5f13a74b316d901b85f7
kawamataryou <ba068082@gmail.com>
2021-01-03 09:26:24 +0900

    fix: api

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 export const fetchCurrentTime = async () => {
-  const res = await fetch('https://gleaming-hickory-iguanacolossus.glitch.me/now')
+  const res = await fetch('https://asia-northeast1-trigger-email-sample.cloudfunctions.net/now')
   const resJson = await res.json()
   return resJson.time
 }
